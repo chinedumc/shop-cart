93e171ff8e582be95a0c4ba7d7280de074e33338
chinedumc <chibokachinedum@gmail.com>
2025-09-09 00:17:59 +0100

    implemented a Product Context

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,12 @@
-import { useState, useEffect } from "react";
+// import { useState, useEffect } from "react";
 import ProductList from "./components/ProductList";
 
 const App = () => {
-	const [products, setProducts] = useState([]);
-	const [loading, setLoading] = useState(true);
-	const [error, setError] = useState(null);
-
-	useEffect(() => {
-		const fetchProducts = async () => {
-			try {
-				const res = await fetch("/api/products");
-				if (!res.ok) throw new Error("Failed to fetch data");
-				const data = await res.json();
-				// console.log(data);
-				setProducts(data);
-			} catch (err) {
-				setError(err.message);
-				// setLoading(false);
-			} finally {
-				setLoading(false);
-			}
-		};
-
-		fetchProducts();
-	}, []);
-
 	return (
 		<div className="min-h-screen bg-gray-100 p-6">
 			<h1 className="text-3xl font-bold mb-6">Product Catalog</h1>
-			{loading && <p>Loading...</p>}
-			{error && <div className="error">{error}</div>}
-			{!loading && !error && <ProductList products={products} />}
+			
+			{ <ProductList />}
 		</div>
 	);
 };
---
+++ src/context/productContext/ProductContext.jsx
@@ -0,0 +1,34 @@
+import {  useEffect, useState } from "react";
+import { ProductContext } from "./productContextFile";
+
+
+export function ProductProvider({ children }) {
+	const [products, setProducts] = useState([]);
+	const [loading, setLoading] = useState(true);
+	const [error, setError] = useState(null);
+
+	useEffect(() => {
+		const fetchProducts = async () => {
+			try {
+				const res = await fetch("/api/products");
+				if (!res.ok) throw new Error("Failed to fetch data");
+				const data = await res.json();
+				// console.log(data);
+				setProducts(data);
+			} catch (err) {
+				setError(err.message);
+				// setLoading(false);
+			} finally {
+				setLoading(false);
+			}
+		};
+
+		fetchProducts();
+	}, []);
+
+	return (
+		<ProductContext.Provider value={{ products, loading, error }}>
+			{children}
+		</ProductContext.Provider>
+	);
+}
