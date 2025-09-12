6b2617ef9faf9f9bb4215b4bd1641c58edae867d
chinedumc <chibokachinedum@gmail.com>
2025-09-12 23:36:44 +0100

    implemented cart context and cart count

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 // import { useState, useEffect } from "react";
 import ProductList from "./components/ProductList";
+import Header from "./components/Header";
 
 const App = () => {
 	return (
-		<div className="min-h-screen bg-gray-100 p-6">
-			<h1 className="text-3xl font-bold mb-6">Product Catalog</h1>
-			
-			{ <ProductList />}
-		</div>
+		<>
+			<Header />
+			<div className="min-h-screen bg-gray-100 p-6">
+				<h1 className="text-3xl font-bold mb-6">Product Catalog</h1>
+
+				{<ProductList />}
+			</div>
+		</>
 	);
 };
 
--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -24,3 +24,4 @@ export function CartProvider({ children }) {
 		</CartContext.Provider>
 	);
 }
+
