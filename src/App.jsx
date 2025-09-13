4c6c17781fce723711190f398d89b4282fb0db07
chinedumc <chibokachinedum@gmail.com>
2025-09-13 23:59:09 +0100

    Added cart dropdown

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,6 @@ const App = () => {
 			<Header />
 			<div className="min-h-screen bg-gray-100 p-6">
 				<h1 className="text-3xl font-bold mb-6">Product Catalog</h1>
-
 				{<ProductList />}
 			</div>
 		</>
